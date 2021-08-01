2251a12ae22dd4ef3ddb9e6648058ae73aad1e56
Alex Novikov <titanium_cheats@hotmail.com>
2021-08-02 00:04:40 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+import pyautogui
+import mss
+import win32gui, win32con
+import vision, recording
+
+print("Starting after 15 sec!")
+pyautogui.sleep(15)
+
+with mss.mss() as sct:
+    # Part of the screen to capture
+    monitor = recording.get_monitor()
+
+    while "Screen capturing":
+        # Get raw pixels from the screen, save it to a Numpy array
+        img = np.array(sct.grab(monitor))
+        img = cv.cvtColor(img, cv.COLOR_RGBA2RGB)
+        
+        cv.imshow("Recording", vision.draw_circle(img))
+
+        #always top most
+        hWnd = win32gui.FindWindow(None, "Recording")
+        win32gui.SetWindowPos(hWnd, win32con.HWND_TOPMOST, 0, 0, 0, 0,
+        win32con.SWP_NOMOVE | win32con.SWP_NOSIZE)
+
+        # Press "q" to quit
+        if cv.waitKey(25) & 0xFF == ord("q"):
+            cv.destroyAllWindows()
+            break
+cv.waitKey()
---
+++ vision.py
@@ -0,0 +1,34 @@
+import pyautogui
+import cv2 as cv
+import numpy as np
+
+"""Computing Vision"""
+#red mask (RGB)
+#low_red = np.flip(np.array([190, 30, 30])) for only day
+low_red = np.flip(np.array([90, 19, 29])) #day + night mode
+high_red = np.flip(np.array([230, 50, 50]))
+boxWeight, boxHeight = (64,64)
+
+#finding a rod float on part of screen
+def find_float(image):
+    global low_red, high_red
+    mask = cv.inRange(image, low_red, high_red)  
+    #get all non zero values
+    coord=cv.findNonZero(mask)
+
+    return coord if not "None" else False
+
+def draw_circle(image):
+    global boxWeight,boxHeight
+    try: 
+        (x_float,y_float) = find_float(image)[0][0]
+        color = (0, 255, 255)
+        thickness = 2
+        image = cv.rectangle(image, (x_float-boxWeight//2, y_float-boxHeight//2), (x_float + 64, y_float + 64), (0,255,255), 1)
+        image = cv.putText(image,"Rod float",(x_float - boxWeight//2, y_float - boxHeight),cv.FONT_HERSHEY_PLAIN,1,(255,255,255),1)
+        return image 
+    except:
+        #print("Not Found")
+        pyautogui.rightClick()
+        pyautogui.sleep(1)
+        return image
